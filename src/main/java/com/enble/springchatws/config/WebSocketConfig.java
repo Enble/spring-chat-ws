73a411d2a54de9bf5fbcffd9f2d209507a557a0a
enble <enble_777@naver.com>
2025-03-11 16:28:10 +0900

    feat: stomp config 설정

---
+++ src/main/java/com/enble/springchatws/config/WebSocketConfig.java
@@ -0,0 +1,33 @@
+package com.enble.springchatws.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+@RequiredArgsConstructor
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    // handshake를 위한 endpoint 지정
+    // cors 설정 포함
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/stomp/chat").setAllowedOrigins("*");
+    }
+
+    // 메모리 기반인 SimpleMessageBroker 활성화
+    // SimpleMessageBroker는 해당 subscribe하는 클라이언트들에게 메시지를 전달하는 간단한 역할 수행
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        // `/sub`가 prefix인 경우 이 메시지 브로커가 경로를 가로채어 처리
+        registry.enableSimpleBroker("/sub");
+
+        // `/pub`이 prefix인 경우 클라이언트의 메시지를 Broker에게 전달
+        // 메시지 전송 시의 경로 설정 메서드
+        registry.setApplicationDestinationPrefixes("/pub");
+    }
+}
