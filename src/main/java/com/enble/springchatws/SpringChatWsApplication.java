bc50ba9e11507734348f33c3eedce52e71a364a0
enble <enble_777@naver.com>
2025-03-11 15:44:35 +0900

    init: initialize project & dependencies
    스프링 부트 초기화 및 의존성을 설정했습니다.
    설정한 의존성은 다음과 같습니다.
    - Web
    - Web Socket
    - stomp-websocket:2.3.4
    - lombok
    
    주석 처리된 의존성은 다음과 같습니다.
    - Data Jpa
    - H2 driver

---
+++ src/main/java/com/enble/springchatws/SpringChatWsApplication.java
@@ -0,0 +1,13 @@
+package com.enble.springchatws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringChatWsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringChatWsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/enble/springchatws/SpringChatWsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enble.springchatws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringChatWsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
